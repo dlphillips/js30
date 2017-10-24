94452dc6aad0bc36a0e313f6b6ffe73bf2ac2c8f
Dan Phillips <dlphillips@gmail.com>
2017-10-23 20:55:30 -0400

    Day 11 - 50%

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,8 +2,8 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progressBar = player.querySelector('progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons =  player.querySelector('[data-skip]');
-const ranges =  player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges =  player.querySelectorAll('.player__slider');
 
 function togglePlay() {
 	if(video.paused) {
@@ -13,5 +13,23 @@ function togglePlay() {
 	}
 }
 
+function updateButton() {
+	const icon = this.paused ? '►' : '❚ ❚';
+ 	toggle.textContent = icon;
+} 
+
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+	console.log(this.value);
+}
+
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('click', handleRangeUpdate));
